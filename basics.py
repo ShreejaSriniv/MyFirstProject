e11c2316447696a1690525909a1d57032fb9dcf4
ShreejaSriniv <168126025+ShreejaSriniv@users.noreply.github.com>
2024-04-25 23:46:42 +0530

    Create basics.py

---
+++ basics.py
@@ -0,0 +1,33 @@
+'''
+What i learnt on day 1:
+int
+string
+list
+tuple
+set
+dict
+'''
+
+a = 1
+print(type(a))
+a = "1"
+print(type(a))
+a = [1, 2, 3, 4,3,2,1]
+print(a)
+print(type(a))
+a.pop(3)
+print(type(a))
+a = (1,2,3,4,4,4)
+print(type(a))
+a = {1,2,3,4,3,2,1}
+print(a)
+print(type(a))
+ShreejaDict = {"Name": "Shreeja", "Age": 18, "Qualification": "Scientist"}
+ShamDict = {"Name": "Tommy", "Age": 52, "Qualification": "Teacher"}
+
+print(ShreejaDict)
+print(ShamDict)
+print(ShreejaDict["Age"])
+
+#b = ["Shreya", "Shreeja", "Sham"]
+#c = [1.1]
