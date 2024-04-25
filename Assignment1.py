4afdc5824125ef28af0344ef71bab6fa2e0663e2
ShreejaSriniv <168126025+ShreejaSriniv@users.noreply.github.com>
2024-04-25 23:56:50 +0530

    Create Assignment1.py

---
+++ Assignment1.py
@@ -0,0 +1,6 @@
+#Assignment Day 1
+
+#WAP to print "Hello world, I am excited to learn python!!"
+#WAP to do all airthmetic operations(+, -, /, *)
+#WAP which will print the type of variables, include(int, string, float, list, set, dict)
+#WAP to concatenate 2 strings, include a constant string
